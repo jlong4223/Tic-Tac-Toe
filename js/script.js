e70a23f1c920b961ee152eeb801d6e31294ada93
jared Long <jlong4223@gmail.com>
2020-10-29 14:11:05 -0500

    completed the game and protected functions

--- js/script.js
+++ js/script.js
@@ -1,74 +1,99 @@
 // alert("js is loaded")
 
-//================ IPO Approach below - Input Process Output ============ //
-// Constant Data - data that doesnt change
+//IIFE Immediately Invoked Function Expression
+(function () {
 
-const COMBOS = [
-    [0, 1, 2], 
-    [3, 4, 5],
-    [6, 7, 8], 
+//================ IPO Approach below - Input Process Output ============ //    
+    // Constant Data - data that doesnt change
 
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
+    const COMBOS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
 
-const LOOKUP = {
-    '1': 'X',
-    '-1': 'O',
-    'null': ''
-};
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
-// Variables (application state)- data that changes
-let turn, winner, gameboard;
+    const LOOKUP = {
+        '1': 'X',
+        '-1': 'O',
+        'null': ''
+    };
 
-
-// Cached element references- elements from html that we are selecting
-const messageEl = document.querySelector('h2');
-const gameboardEl = document.getElementById('gameboard');
-const squareEls = document.querySelectorAll('.square');
-const buttonEl = document.querySelector('button');
+    // Variables (application state)- data that changes
+    let turn, winner, gameboard;
 
 
-// Event listeners
-gameboardEl.addEventListener('click', handleClick);
-buttonEl.addEventListener('click', init);
+    // Cached element references- elements from html that we are selecting
+    const messageEl = document.querySelector('h2');
+    const gameboardEl = document.getElementById('gameboard');
+    const squareEls = document.querySelectorAll('.square');
+    const buttonEl = document.querySelector('button');
 
 
-// Functions
+    // Event listeners
+    gameboardEl.addEventListener('click', handleClick);
+    buttonEl.addEventListener('click', init);
+
+
+    // Functions
     //start the game upon initial load and whenever the reset button is clicked
-init();
+    init();
 
-function init(){
-    //pick and set turn
-    turn = 1; // X will go first
-    //set winner to false
-    winner = false;
-    //set up the in-memory gameboard to empty
-    // gameboard = [null, null, null, null, null, null, null, null, null];
-    gameboard = new Array(9).fill(null);    //this is the same as line above
-    //visualize a new game to the DOM
-    render();
-};
+    function init() {
+        //pick and set turn
+        turn = 1; // X will go first
+        //set winner to false
+        winner = false;
+        //set up the in-memory gameboard to empty
+        // gameboard = [null, null, null, null, null, null, null, null, null];
+        gameboard = new Array(9).fill(null); //this is the same as line above
+        //visualize a new game to the DOM
+        render();
+    };
 
-//.dataset is referring to the data identifyer in the html- so when you click on a box, it will consolelog the index number of the box you clicked
+    function checkWinner() {
+        //needs to loop over the combos array and check each collection of combination values against corresponding values inside gameboard array
+        for (let i = 0; i < COMBOS.length; i++) {
+            if (Math.abs(gameboard[COMBOS[i][0]] +
+                    gameboard[COMBOS[i][1]] +
+                    gameboard[COMBOS[i][2]]) === 3) {
+                return gameboard[COMBOS[i][0]];
+            }
+        }
+        if (gameboard.includes(null)) return false;
+        return "Tie";
+    }
 
-function handleClick(event) {
-    // console.log(event.target.dataset.index)
-    const position = event.target.dataset.index;
-    if(gameboard[position] !== null) return; //this stops ability to change x/o 
-    gameboard[position] = turn;
-    turn *= -1;
-    render();
-    // console.log(gameboard)
-};
+    //.dataset is referring to the data identifyer in the html- so when you click on a box, it will consolelog the index number of the box you clicked
 
-function render() {
-    //transfer the state of the game to the DOM
-    squareEls.forEach(function(square, position){
-        square.textContent = LOOKUP[gameboard[position]];
-    });
-    messageEl.textContent = `player ${LOOKUP[turn]}'s turn`
-};
+    function handleClick(event) {
+        // console.log(event.target.dataset.index)
+        const position = event.target.dataset.index;
+        if (winner || gameboard[position] !== null) return; //this stops ability to change x/o 
+        gameboard[position] = turn;
+        turn *= -1;
+        winner = checkWinner();
+        render();
+        // console.log(gameboard)
+    };
+
+    function render() {
+        //transfer the state of the game to the DOM
+        squareEls.forEach(function (square, position) {
+            square.textContent = LOOKUP[gameboard[position]];
+        });
+        if (!winner) {
+            messageEl.textContent = `player ${LOOKUP[turn]}'s turn`
+        } else if (winner === 'Tie') {
+            messageEl.textContent = `Tie Game`
+        } else {
+            messageEl.textContent = `Player ${LOOKUP[winner]} Wins`
+        }
+    };
+
+})()
